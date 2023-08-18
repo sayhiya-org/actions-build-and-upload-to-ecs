5ea4cd08c1a70dad3374491dae9f44a7fe69cb07
Alex <64654623+alex-96-eng@users.noreply.github.com>
2023-08-18 17:02:03 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ function main() {
   sanitize "${INPUT_SECRET_ACCESS_KEY}" "secret_access_key"
   sanitize "${INPUT_REGION}" "region"
   sanitize "${INPUT_ACCOUNT_ID}" "account_id"
-  sanitize "${INPUT_REPO}" "repo"
 
   ACCOUNT_URL="$INPUT_ACCOUNT_ID.dkr.ecr.$INPUT_REGION.amazonaws.com"
 
@@ -31,36 +30,20 @@ function aws_configure() {
 }
 
 function login() {
-  echo "== START LOGIN"
+  echo "Attempting to log into AWS"
   LOGIN_PASSWORD=$(aws ecr get-login-password --region $AWS_DEFAULT_REGION)
   echo "$LOGIN_PASSWORD" | docker login --username AWS --password-stdin $ACCOUNT_URL
-  echo "== FINISHED LOGIN"
-}
-
-function create_ecr_repo() {
-  if [ "${1}" = true ]; then
-    echo "== START CREATE REPO"
-    aws ecr describe-repositories --region $AWS_DEFAULT_REGION --repository-names $INPUT_REPO > /dev/null 2>&1 || \
-      aws ecr create-repository --region $AWS_DEFAULT_REGION --repository-name $INPUT_REPO
-    echo "== FINISHED CREATE REPO"
-  fi
+  echo "Login Successful!"
 }
 
 function docker_build() {
-  echo "== START DOCKERIZE"
-  local TAG=$1
-  local docker_tag_args=""
-  local DOCKER_TAGS=$(echo "$TAG" | tr "," "\n")
-  for tag in $DOCKER_TAGS; do
-    docker_tag_args="$docker_tag_args -t $2/$INPUT_REPO:$tag"
-  done
-
-  docker build . --build-arg $INPUT_EXTRA_BUILD_ARGS -f $INPUT_DOCKERFILE t $docker_tag_args   
-  echo "== FINISHED DOCKERIZE"
+  echo "Attempting to build docker image"
+  docker build --build-arg $INPUT_EXTRA_BUILD_ARGS -f $INPUT_DOCKERFILE -t $docker_tag_args   
+  echo "Build Successful!"
 }
 
 function docker_push_to_ecr() {
-  echo "== START PUSH TO ECR"
+  echo "Attempting to push docker image"
   local TAG=$1
   local DOCKER_TAGS=$(echo "$TAG" | tr "," "\n")
   for tag in $DOCKER_TAGS; do
